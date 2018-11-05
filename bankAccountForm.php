f548be50d69a88dd68290d81da2f073edba2a3f5
Unknown <duhalgouetm@yahoo.com>
2018-11-05 17:09:46 +0100

    Story 2

---
+++ bankAccountForm.php
@@ -0,0 +1,16 @@
+<form method='POST' action='traitement.php'>
+	
+	<input type="text" name="nameBankAccount">
+	<select name="typeAccount">
+		<option value='epargne'>Epargne</option>
+		<option value='courant'>Courant</option>
+		<option value='compte joint'>Compte joint</option>
+	</select>
+	<input type="number" name="provision">
+	<select name='currency'>
+		<option value='USD'>USD</option>
+		<option value='EUR'>EUR</option>
+	</select>
+	<input type="submit" name="submitFormAccount" value="Valider">
+</form>
+
---
+++ traitement.php
@@ -0,0 +1,22 @@
+<?php
+	function verifFormBankAccount()
+	{
+		if(isset($_POST['submitFormAccount']))
+		{
+			if(strlen($_POST['nameBankAccount']) > 50) && (!htmlspecialchars($_POST['nameBankAccount']))
+			{
+				echo 'Nom limité à 50 caracteres veuillez rentrer un nom conforme';
+			}
+			else if ($_POST['typeAccount'] != ('epargne' || 'compte joint' || 'courant'))
+			{
+				echo "Choix Impossible";
+			}
+			elseif (!is_numeric ($_POST['provision'])) 
+			{
+				echo 'Ecrivez un nombre'
+			}
+
+		
+		}	
+	}
+?>
